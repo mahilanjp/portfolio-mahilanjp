c0f545025541aa5de94f1e6f7d9aa2fa5a2b0f81
mahilanjp <mahilan.jp@gmail.com>
2025-01-05 23:10:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,6 +27,41 @@ function myMenuFunction(){
 
     }
   }
+  //Blinking Text
+  document.addEventListener("DOMContentLoaded", function () {
+    function writeText(element, content) {
+      let contentArray = content.split(""),
+          current = 0;
+      
+      let interval = setInterval(function () {
+        if (current < contentArray.length) {
+          element.textContent += contentArray[current++];
+        } else {
+          clearInterval(interval);
+        }
+      }, 100);
+    }
 
-  //https://devsnap.me/css-mobile-menus
-  //https://github.com/webdevcreative/responsive-navbar-design
+    const element = document.getElementById("mainpage-holder");
+    writeText(element, "WEB DESIGNER + FRONT-END DEVELOPER");
+  });
+  //Scroll Bar
+  document.addEventListener("DOMContentLoaded", function () {
+    const links = document.querySelectorAll('a[href*="#"]');
+  
+    links.forEach(function (link) {
+      link.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        const targetId = link.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        
+        if (targetElement) {
+          window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: 'smooth'
+          });
+        }
+      });
+    });
+  });
